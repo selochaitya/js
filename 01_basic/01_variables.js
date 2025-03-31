9761e5536af5bc1b09d470a2748a2a56fefc35b7
Chaitanya Selokar <132004847+selochaitya@users.noreply.github.com>
2025-03-31 19:16:18 +0000

    container config

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,20 @@
+const empId=111;
+let empName="chaitya";
+var empPassword="123456";
+empAddress="Nagpur";
+let empState;
+
+console.table([empId,empName,empPassword]);
+
+//after change
+/*empId="101";
+not allowed
+
+empState=undefined
+*/
+
+empName="chaitanya";
+empPassword="123";
+empAddress="pune";
+
+console.table([empId,empName,empPassword,empState]);
